1d0ba06063ff59716984ab4dc946d8506fcab127
Banana <lbsrex@gmail.com>
2017-09-12 01:23:41 +0530

    Started the python basics track.

---
+++ Basics/001-Hello.py
@@ -0,0 +1,3 @@
+#Just printing a greating. 
+#No formalities. With python, things just work out without any extra effort.
+print 'Hello MoFo(s)'
---
+++ Basics/002-Maths.py
@@ -0,0 +1,28 @@
+#importing libraries helps you use code someone else has written. You can even give them nicknames.
+import math as billy
+
+#featuring :
+
+#addition
+print 22+47
+#subtraction
+print 102-33
+#multiplication
+print 23*3
+#division
+print 207/3
+#modulus(remainder)
+
+#power
+print 2**4
+
+
+#We also have :
+
+#ceiling
+print billy.ceil(2.134)
+#factorial
+print billy.factorial(4)
+
+
+#for more on the math lib : https://docs.python.org/2/library/math.html
